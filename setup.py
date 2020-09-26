5b56ebe87415ec2d207da42f48febb147ca5ae92
lawrencecollins <67695762+lawrencecollins@users.noreply.github.com>
2020-09-26 12:04:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="calciumflexanalysis", 
-    version="1.0.0",
+    version="1.0.1",
     author="Lawrence Collins, Stuart Warriner",
     author_email="s.l.warriner@leeds.ac.uk, lawrencejordancollins@gmail.com",
     description="Processing and analysis of calcium flex assays",
